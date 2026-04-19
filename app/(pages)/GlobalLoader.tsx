883a5bd634f0326615926719ec997dc68fca31b5
gulbin-dev <gulbindev@gmail.com>
2026-04-19 18:11:21 +0800

    refractor: added text for additional information

--- app/(pages)/GlobalLoader.tsx
+++ app/(pages)/GlobalLoader.tsx
@@ -25,6 +25,7 @@ export default function GlobalLoader() {
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-action-color mx-auto mb-4"></div>
           <p className="text-lg text-foreground">Loading page...</p>
+          <p className="mt-1">First time visit may take a while to load</p>
         </div>
       </div>
     )
