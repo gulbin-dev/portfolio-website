8a7ccdd47952c2d9200d21b7b7f6618561340934
gulbin-dev <gulbindev@gmail.com>
2026-04-19 17:08:09 +0800

    fix: image dimension

--- app/(pages)/_components/CardSkill.tsx
+++ app/(pages)/_components/CardSkill.tsx
@@ -61,7 +61,7 @@ export default function CardSkill() {
                 src="/accessibility.png"
                 alt=""
                 width={800}
-                height={500}
+                height={450}
                 className="tablet:max-w-80 desktop:max-w-100 object-cover aspect-video"
               />
               <div className="container-video"></div>
