299e53a867e31f86c1d2153e0772e622f80c2ef0
gulbin-dev <gulbindev@gmail.com>
2026-04-21 20:35:24 +0800

    refractor: remove excessive ScrollTrigger.refresh calculation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import NavLinks from "./NavLinks";
 import Link from "next/link";
 import Image from "next/image";
-import useHeaderTransition from "@hooks/useHeaderTransition";
 import { useLoading } from "../utils/LoadingContext";
+import { gsap, ScrollTrigger, useGSAP } from "@utils/gsap";
 
 export default function Header() {
-  useHeaderTransition();
   const { isRevealed } = useLoading();
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
   useEffect(() => {
     if (isMobileNavOpen) {
       document.body.style.overflow = "hidden";
@@ -18,14 +18,45 @@ export default function Header() {
       document.body.style.overflow = "auto";
     };
   }, [isMobileNavOpen]);
+
+  useGSAP(() => {
+    let isScrollingDown = true;
+    const createHeaderAnimation = () => {
+      const showHeaderAnim = gsap.to("#header", {
+        yPercent: -100,
+        duration: 0.5,
+        paused: true,
+        id: "showHeaderAnim",
+      });
+      ScrollTrigger.create({
+        animation: showHeaderAnim,
+        start: 0,
+        end: "max",
+        onUpdate: (self) => {
+          const velocity = self.getVelocity();
+          if (Math.abs(velocity) < 15) return;
+
+          if (velocity < 0 && isScrollingDown) {
+            isScrollingDown = false;
+            showHeaderAnim.reverse();
+          } else if (velocity > 0 && !isScrollingDown) {
+            isScrollingDown = true;
+            showHeaderAnim.play();
+          }
+        },
+      });
+    };
+    createHeaderAnimation();
+  }, []);
+
   return (
     isRevealed && (
       <>
         <header
           id="header"
-          className="fixed! z-200 left-0 top-0 w-full  max-h-12 tablet:max-h-9 bg-secondary-color "
+          className="fixed! z-200 left-0 top-0 w-full min-h-12 content-center  tablet-portrait:min-h-9 bg-secondary-color "
         >
-          <div className="max-w-180 w-full h-full flex justify-between items-center place-self-center px-3 py-3 tablet:py-0">
+          <div className="max-w-180 w-full h-full flex justify-between items-center place-self-center px-3 py-3 tablet-portrait:py-0">
             <Link href="/" className="px-0 text-white">
               <Image
                 src="/logo.svg"
@@ -35,10 +66,10 @@ export default function Header() {
                 loading="eager"
               />
             </Link>
-            <NavLinks navStyle="hidden header-nav gap-6 mr-8 tablet:flex" />
+            <NavLinks navStyle="hidden header-nav gap-6 mr-8 tablet-portrait:flex" />
             <button
               onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
-              className="flex flex-col gap-1 tablet:hidden"
+              className="flex flex-col gap-1 tablet-portrait:hidden"
               aria-label="Navigation menu"
             >
               <span className="hamburger-icon"></span>
