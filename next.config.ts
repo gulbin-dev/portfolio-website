2f0a70f365c7946437168a6e5fa1107a7a2dc138
gulbin-dev <gulbindev@gmail.com>
2026-04-22 17:58:05 +0800

    refractor: extend image size optimazation

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     deviceSizes: [320, 330, 380, 420, 450, 480, 768, 1080, 1200, 1400],
-    imageSizes: [250, 280, 300, 370, 700, 800, 900, 1000, 1200, 1400],
+    imageSizes: [100, 250, 280, 300, 370, 700, 800, 900, 1000, 1200, 1400],
     remotePatterns: [
       {
         protocol: "https",
