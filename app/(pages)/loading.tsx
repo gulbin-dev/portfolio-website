e30c99673d60271e9cf426808ea4da26ef224ee8
gulbin-dev <gulbindev@gmail.com>
2026-04-28 10:49:30 +0800

    refractor: adjust stack hierarchy

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="loader-bg min-h-screen fixed min-w-screen inset flex items-center justify-center bg-linear-to-br from-primary-color to-primary-color-darker overflow-hidden">
+    <div className="loader-bg min-h-screen fixed min-w-screen inset flex items-center z-60 justify-center bg-linear-to-br from-primary-color to-primary-color-darker overflow-hidden">
       <div className="text-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-action-color mx-auto mb-4"></div>
         <p className="text-lg text-foreground">Loading page...</p>
